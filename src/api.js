d3771fe5604dabd3b11158908fc0a39b73045a3a
David Stephens <stephens.a.david@gmail.com>
2019-11-15 11:34:23 -0600

    updated for books

--- src/api.js
+++ src/api.js
@@ -1,6 +1,36 @@
 module.exports = {
-  getMovies: () => {
-    return fetch('/api/movies')
+  getBooks: () => {
+    return fetch('/api/books')
       .then(response => response.json());
+  },
+  getBook : (id) => {
+    return fetch(`/api/books/${id}`)
+        .then(resp => resp.json());
+  },
+  postBook : (book) => {
+    return fetch('api/books', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(book),
+    })
+  },
+  patchBook : (book, id) => {
+    return fetch(`api/books/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(book),
+    })
+  },
+  deleteBook : (id) => {
+    return fetch(`api/books/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
   }
 };
--- src/index.js
+++ src/index.js
@@ -7,14 +7,73 @@ sayHello('World');
 /**
  * require style imports
  */
-const {getMovies} = require('./api.js');
+const {getBook, getBooks, postBook, patchBook, deleteBook} = require('./api.js');
 
-getMovies().then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+getBooks().then((books) => {
+  console.log('Here are all the books:');
+  books.forEach(({title, author, year}) => {
+    console.log(`${title} by ${author} - ${year}`);
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
+  console.log(error);
+});
+
+getBook(1)
+    .then(book => {
+      console.log("Making a request to a single book");
+      console.log(`${book.title} by ${book.author} - ${book.year}`);
+    })
+    .catch(() => console.log('The important thing is you tried...'));
+
+// postBook({
+//   "author": "Jim Davis",
+//   "country": "United States",
+//   "imageLink": "images/???.jpg",
+//   "language": "English",
+//   "link": "https://www.amazon.com/Garfield-Loses-His-Feet-Book/dp/0345464672\n",
+//   "pages": 98,
+//   "title": "Garfield Loses His Feet",
+//   "year": 1984
+// }).then(getBooks).then((books) => {
+//   console.log('Here are all the books:');
+//   books.forEach(({title, author, year}) => {
+//     console.log(`${title} by ${author} - ${year}`);
+//   });
+// }).catch((error) => {
+//   alert('Oh no! Something went wrong.\nCheck the console for details.');
+//   console.log(error);
+// });
+
+// patchBook({
+//   "pages": 1201,
+//   "title": "Garfield Learns Python III",
+//   "year": 2023
+// }, 26).then(getBooks).then((books) => {
+//   console.log('Here are all the books:');
+//   books.forEach(({title, author, year}) => {
+//     console.log(`${title} by ${author} - ${year}`);
+//   });
+// }).catch((error) => {
+//   alert('Oh no! Something went wrong.\nCheck the console for details.');
+//   console.log(error);
+// });
+
+deleteBook(27).then(postBook({
+  "author": "Jim Davis",
+  "country": "United States",
+  "imageLink": "images/???.jpg",
+  "language": "English",
+  "link": "https://www.amazon.com/Garfield-Loses-His-Feet-Book/dp/0345464672\n",
+  "pages": 98,
+  "title": "Garfield Loses His Feet",
+  "year": 1984
+})).then(getBooks).then((books) => {
+  console.log('Here are all the books:');
+  books.forEach(({title, author, year}) => {
+    console.log(`${title} by ${author} - ${year}`);
+  });
+}).catch((error) => {
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
